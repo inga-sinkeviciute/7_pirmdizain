fd338de916659996822639a3f6c7205a15802666
Inga Sinkeviciute <isinkeviciute7@gmail.com>
2023-10-11 09:26:46 +0300

    some additions

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 # 7_pirmdizain
-Look at my picture. It's glorious
 
-https://inga-sinkeviciute.github.io/7_pirmdizain/
+## dizain dizain
+
+### dizaininskas
+
+#### dizainovicius
+
+Look at my picture. It's glorious [click me]
+(https://inga-sinkeviciute.github.io/7_pirmdizain/)
--- script.js
+++ script.js
@@ -5,3 +5,25 @@ document.addEventListener("DOMContentLoaded", function () {
 		window.open("https://home.pl/", "_blank");
 	});
 });
+
+function toggleMode() {
+	const linkElement = document.getElementById("style-link");
+	const themeToggle = document.getElementById("theme-toggle");
+
+	// Check if the current stylesheet is light mode
+	const isLightMode = linkElement.href.endsWith("styles-light.css");
+
+	themeToggle.addEventListener("click", () => {
+		if (isLightMode) {
+			linkElement.href = "dark.css"; // Switch to dark mode
+		} else {
+			linkElement.href = "style.css"; // Switch to light mode
+		}
+
+		// Update the mode flag
+		isLightMode = !isLightMode;
+	});
+}
+
+// Call toggleMode on page load
+window.addEventListener("DOMContentLoaded", toggleMode);
